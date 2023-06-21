632ac7b38ffb339591d023b0618c48641b5b7460
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-06-21 17:56:29 -0300

    pipeline - names comparator

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Product;
 
@@ -37,6 +39,15 @@ public class Program {
 			
 			System.out.println("Average price: " + String.format("%.2f", avg));
 			
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			List<String> names = list.stream()
+					.filter(p -> p.getPrice() < avg)
+					.map(p -> p.getName())
+					.sorted(comp.reversed())
+					.collect(Collectors.toList());
+			
+			names.forEach(System.out::println);
 			
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
